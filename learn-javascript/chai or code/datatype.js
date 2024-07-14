f0c8cf00fd4cedef82187f57da7c4408b734ac61
2705jagriti <jagritichaudhary842@gmail.com>
2024-07-14 21:40:26 +0530

    .

---
+++ learn-javascript/chai or code/datatype.js
@@ -0,0 +1,15 @@
+var number = 10;
+console.log(number);
+let userName = "jagriti";
+console.log(userName);
+const userId = true;
+console.log(userId);
+let location = false;
+// let salary = nulll;
+console.log(typeof ( location, userName));
+const car = ["volvo", "BMW", "range rover "];
+console.log(car);
+userName = "chaudhray";
+console.log(userName);
+userId = false;
+console.log(userId);
