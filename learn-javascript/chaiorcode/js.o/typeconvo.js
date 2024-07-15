9e3162ce3b313d69697aacbf9e795b5f42465f99
2705jagriti <jagritichaudhary842@gmail.com>
2024-07-15 20:38:03 +0530

    conversion of datatype

---
+++ learn-javascript/chaiorcode/js.o/typeconvo.js
@@ -0,0 +1,13 @@
+//  let value=null;
+//  console.log( typeof value);
+//  value = Number(value)
+//  console.log(value);
+//  value="33abc"
+//  value = Number(value)
+//  console.log(typeof value); 
+//  console.log(value);
+ let isLog = 6379;
+ isLog = String(isLog);
+ console.log(typeof isLog);
+console.log(isLog);
+//null,undefined==false /string ,number ==true
