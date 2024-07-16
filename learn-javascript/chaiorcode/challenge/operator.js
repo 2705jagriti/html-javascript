8b9a7cc12cdd155e6bc043d6523bdf0f551e69bb
2705jagriti <jagritichaudhary842@gmail.com>
2024-07-16 14:44:43 +0530

    operations

---
+++ learn-javascript/chaiorcode/challenge/operator.js
@@ -0,0 +1,12 @@
+//addition 
+let a= 10 , b = 20;
+console.log(a+b);
+// sub
+console.log(a-b);//sub
+console.log(a*b);//multiply
+console.log(a/b);
+console.log(a%b);
+
+//operatop 
+let c= 40;
+console.log();
---
+++ learn-javascript/chaiorcode/js.o/operation.js
@@ -0,0 +1,19 @@
+//operation string to number
+let value = 3;
+let negvalue = -value;
+// console.log(negvalue);
+// console.log(2**3);
+//memory allocation
+let userName = "jagritichaudhary"
+let anotherName = userName;
+anotherName= "swatichaudhary"
+// console.log(userName);
+// console.log(anotherName);
+let cars = ["volvo","bmw" ,"inova"]
+console.log(cars);
+cars[0]="belino";
+console.log(cars);
+let cars2 = cars;
+cars2[1]="ola"
+console.log(cars);
+console.log(cars2);
