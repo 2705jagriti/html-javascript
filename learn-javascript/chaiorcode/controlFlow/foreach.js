878093bcb6288e62c61a0232294a1c0da9dc4d49
2705jagriti <jagritichaudhary842@gmail.com>
2024-07-25 22:49:27 +0530

    ..

---
+++ learn-javascript/chaiorcode/controlFlow/foreach.js
@@ -0,0 +1,11 @@
+// const language = ["js" , "java" ,"ruby" , "python"]
+// const value = language.forEach( (item) =>{
+//    console.log(item);
+//     return item;
+// } )
+// console.log(value);
+
+
+// xxxxxxxxxxx FOR EACH XXXXXXXXX
+const num =[1,2,3,4,5,6,7,8,9,10];
+num.filter()
