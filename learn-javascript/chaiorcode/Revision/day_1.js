1390e4400b882f70a41958a8286b45e7a21d44e4
2705jagriti <jagritichaudhary842@gmail.com>
2024-07-27 23:23:42 +0530

    .

--- learn-javascript/chaiorcode/Revision/day_1.js
+++ learn-javascript/chaiorcode/Revision/day_1.js
@@ -3,3 +3,9 @@ let arr = [1, 2, 3, 4, 5]; // in js
 // int [] arr = {1,2,3,4,5}   IN JAVA
 // in js we can have different type of datatype and resizable
 console.log(arr[3]);
+// xxxxx  UPSHIFT IS A METHOD WHICH ADD VALUE AT [0] , AND SHIFT DELETE THE [0]
+console.log(arr.includes(8));
+console.log(arr);
+const newarr = arr.join(); //typeof == string
+console.log(newarr);
+// SLICE OR SPLICE
