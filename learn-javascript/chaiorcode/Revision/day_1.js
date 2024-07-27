662c507938099e5507f72695cb8f96ac1caac0d4
2705jagriti <jagritichaudhary842@gmail.com>
2024-07-27 23:14:27 +0530

    ..

---
+++ learn-javascript/chaiorcode/Revision/day_1.js
@@ -0,0 +1,5 @@
+//xxxx START WITH ARRAY XXXXX
+let arr = [1, 2, 3, 4, 5]; // in js
+// int [] arr = {1,2,3,4,5}   IN JAVA
+// in js we can have different type of datatype and resizable
+console.log(arr[3]);
