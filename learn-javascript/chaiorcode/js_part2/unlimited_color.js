04602f5078ce9144c04708716f1e86170fa675f1
2705jagriti <jagritichaudhary842@gmail.com>
2024-07-31 00:27:47 +0530

    .

--- learn-javascript/chaiorcode/js_part2/unlimited_color.js
+++ learn-javascript/chaiorcode/js_part2/unlimited_color.js
@@ -1,1 +1,7 @@
-document.getElementById('#start').style.Color= "green"
+ const randomValue = function(){
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for (let  i =0 ; i<6 ;i++) {
+       console.log(Math.floor(Math.random()*16));
+    }
+ }
