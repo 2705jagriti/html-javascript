44dde09f27a857a5020f1131ef428de71c4fd20b
2705jagriti <jagritichaudhary842@gmail.com>
2024-07-31 00:46:27 +0530

    ..

--- learn-javascript/chaiorcode/js_part2/unlimited_color.js
+++ learn-javascript/chaiorcode/js_part2/unlimited_color.js
@@ -1,7 +1,24 @@
+  const heading = document.querySelector('h1')
+  heading.style.backgroundColor = "gray"
  const randomValue = function(){
     const hex = "0123456789ABCDEF"
     let color = '#'
     for (let  i =0 ; i<6 ;i++) {
-       console.log(Math.floor(Math.random()*16));
+      color +=hex[(Math.floor(Math.random()*16))]
+    }
+    return color;
+ };
+ let interval;
+ const startcolor = function(){
+    interval=setInterval(selectcolor, 1000)
+    function selectcolor(){
+        document.body.style.background= randomValue();
     }
  }
+
+ const stopcolor = function(){
+    clearInterval(interval)
+ }
+
+ document.getElementById('start').addEventListener('click' , startcolor); 
+ document.getElementById('stop').addEventListener('click' , stopcolor); 
